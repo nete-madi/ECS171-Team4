3cd573cee44570e2ea5e9e7c16395a57d19af6c3
Tycho Yacub <tychoyacub@gmail.com>
2020-11-30 22:19:47 -0800

    Invert, threshold, then crop the image as a final step of processing

--- ECS171Backend/CoreBackend/image_processing.py
+++ ECS171Backend/CoreBackend/image_processing.py
@@ -1,12 +1,12 @@
 import cv2
-
+import numpy as np
 
 from CoreBackend import box_extraction as be
 from CoreBackend import page_recognition as pr
 
 def main():
     pass
-    # import numpy as np
+    #
     #
     # image_name = 'test2.png'
     # boxes = process_image(image_name)
@@ -19,10 +19,35 @@ def process_image(image_path):
     page = cv2.cvtColor(page, cv2.COLOR_BGR2GRAY)
 
     boxes = be.get_boxes(page)
+    boxes = [cv2.bitwise_not(box) for box in boxes]
+    boxes = [cv2.threshold(box, 127, 255, cv2.THRESH_BINARY)[1] for box in boxes]
+
+    # Zoom into digit/letter
+    # boxes = [cv2.GaussianBlur(box, (5,5),0) for box in boxes]
+    boxes = [crop_img(box) for box in boxes]
+
     boxes = [cv2.resize(box, (28,28)) for box in boxes]
-    boxes = [box.reshape((28,28,1)) for box in boxes]
+
+
+    boxes = [np.reshape(box, (28,28,1)) for box in boxes]
+
 
     return boxes
+
+def crop_img(img):
+    coords = cv2.findNonZero(img)  # Find all non-zero points (text)
+    if coords is None: return np.zeros((28,28))
+    x, y, w, h = cv2.boundingRect(coords)  # Find minimum spanning bounding box
+
+    if w > h:
+        out = np.zeros((w,w))
+        out[int(w / 2 - h / 2):int(w / 2 + h / 2), :] = img[y:y + h, x:x + w]
+        return out
+    else:
+        out = np.zeros((h,h))
+        out[:, int(h / 2 - w / 2):int(h / 2 + w / 2)] = img[y:y + h, x:x + w]
+        return out
+
 
 if __name__ == "__main__":
     main()
